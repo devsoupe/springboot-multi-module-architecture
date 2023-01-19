2c75d4b68a9f00935ea43a1db480f14aa7c86986
devsoupe <luvyum4e@gmail.com>
2023-01-19 18:24:47 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/perelandrax/smma/springbootmultimodulearchitecture/SpringbootMultiModuleArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package com.perelandrax.smma.springbootmultimodulearchitecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootMultiModuleArchitectureApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringbootMultiModuleArchitectureApplication>(*args)
+}
---
+++ src/test/kotlin/com/perelandrax/smma/springbootmultimodulearchitecture/SpringbootMultiModuleArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.perelandrax.smma.springbootmultimodulearchitecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootMultiModuleArchitectureApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
